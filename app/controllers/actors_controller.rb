905a51f9375dfbc4d48f7d18a9913237f0e1d2d0
John Leavell <johnleavell@gmail.com>
2023-04-13 14:04:25 -0500

    added actor and movie RCAV

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,8 @@
+class ActorsController < ApplicationController
+
+  def index
+    @list_of_actors = Actor.all
+
+    render({ :template => "actor_templates/index.html.erb"})
+  end
+end
---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,8 @@
+class MoviesController < ApplicationController
+
+  def index
+    @list_of_movies = Movie.all
+    render({ :template => 'movie_templates/index.html.erb' })
+  end
+
+end
